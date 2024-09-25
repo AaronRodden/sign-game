6334ac09e46ddbc77ca9c34def684cee655e1741
Aaron Rodden <aaron.rodden21@gmail.com>
2024-09-25 16:42:07 -0700

    Initial commit with basic moving game

---
+++ Main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# Testing ground for my capstone / TUI Game.
+# The intention of this particular repo is to be a sandbox for game mechanics and tech.
+# As a secondary goal I want to use this repo as a way to learn about proper Godot code architecture.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ player.gd
@@ -0,0 +1,63 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+var lookvector = Vector2.ZERO
+var pickaxe_position = Vector2.ZERO
+
+var tilemap
+var cell
+var tile_id
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _ready():
+	tilemap = get_parent().get_node("World")
+	
+func _process(delta):
+	
+	# TODO: Don't love how this is done, weird mix of pixel based vs tile coordinate based
+	# Use map to local?
+	if Input.is_action_pressed("right"):
+		$Pointer.position = Vector2(50,0)
+		pickaxe_position = position + Vector2(50,0)
+	if Input.is_action_pressed("left"):
+		$Pointer.position = Vector2(-50,0)
+		pickaxe_position = position + Vector2(-50,0)
+	if Input.is_action_pressed("up"):
+		$Pointer.position = Vector2(0,-50)
+		pickaxe_position = position + Vector2(0,-50)
+	if Input.is_action_pressed("down"):
+		$Pointer.position = Vector2(0,50)
+		pickaxe_position = position + Vector2(0,50)
+		
+	cell = tilemap.local_to_map(pickaxe_position)
+	tile_id = tilemap.get_cell_tile_data(0, cell)
+	
+	print(cell)
+	print(tile_id)
+	
+	if Input.is_action_just_pressed("R1"):
+		tilemap.erase_cell(0, cell)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("square") and is_on_floor():
+		velocity.y = JUMP_VELOCITY 
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
