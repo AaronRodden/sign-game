df0e9398720bd5ff73a3345e87778065d78a49b2
Aaron Rodden <aaron.rodden21@gmail.com>
2024-09-26 18:17:41 -0700

    quick camera implementation

---
+++ camera.gd
@@ -0,0 +1,22 @@
+@tool
+extends Camera2D
+
+@export var screenSize = Vector2(1152, 648)
+@export var player : CharacterBody2D
+var startOffsetPosition : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	startOffsetPosition = global_position
+
+func _physics_process(_delta):
+	UpdateCamera()
+
+func UpdateCamera():
+	var currentCell = ((player.global_position - startOffsetPosition) / screenSize).floor()
+	print(currentCell)
+	global_position = startOffsetPosition + (currentCell * screenSize)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
